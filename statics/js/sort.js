5c6c879afd94c95c04f02f222dd97d9b67632ed9
yanlv.yin <yanlv.yin@servision.com.cn>
2015-11-20 18:28:50 +0800

    增加算法js实现

---
+++ statics/js/sort.js
@@ -0,0 +1,59 @@
+/**
+ * 交换排序——冒泡排序
+ *
+ * @param {Array} arr
+ * @return {Array}
+ */
+function bubbleSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length - i; j++) { //i,已下沉的元素个数
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+/**
+ * 交换排序——快速排序
+ *
+ * @param {Array} arr
+ * @returns {Array}
+ */
+function quickSort(arr) {
+    var leftArr = [];
+    var rightArr = [];
+    var divider;
+    if (arr.length === 0) {
+        return [];
+    } else if (arr.length === 1) {
+        return arr;
+    }
+    divider = arr[0];
+    for (var i = 1;  i < arr.length; i++) { //从第二个元素开始,注意arr.length，防止当数组只有两个元素，不进入循环
+        arr[i] < divider ? leftArr.push(arr[i]) : rightArr.push(arr[i]);
+    }
+    return quickSort(leftArr).concat(divider, quickSort(rightArr));
+}
+
+/**
+ * 基数排序——桶排序
+ *
+ * @param {Array} arr
+ * @return {Array}
+ */
+function bucketSort(arr) {
+
+
+    return arr;
+}
+
+
+
+
+
+
+
